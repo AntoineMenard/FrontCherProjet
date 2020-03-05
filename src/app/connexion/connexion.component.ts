13c133d588fec25d04ba3dc3340b971a3ad2d927
Enyram <maryne.bensacoun@gmail.com>
2020-03-05 16:54:23 +0100

    modif form connexion et inscription

--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Utilisateur } from '../model/Utilisateur';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-connexion',
@@ -11,7 +12,9 @@ export class ConnexionComponent implements OnInit {
   
   Utilisateur: Utilisateur = new Utilisateur();
 
-  constructor() { }
+  constructor(private router: Router) { }
+  particulier = false;
+  entreprise = false;
 
   ngOnInit(): void {
   }
@@ -19,6 +22,26 @@ export class ConnexionComponent implements OnInit {
   Connexion() {
       localStorage.setItem(this.Utilisateur.mail, this.Utilisateur.mdp);
       return true;
+    }
+
+    visibleParticulier() {
+      if (this.particulier) {
+        this.particulier = false;
+  
+      } else {
+        this.particulier = true;
+        this.entreprise = true;
+        this.visibleEntreprise();
+      }
+    }
+    visibleEntreprise() {
+      if (this.entreprise) {
+        this.entreprise = false;
+      } else {
+        this.entreprise = true;
+        this.particulier = true;
+        this.visibleParticulier();
+      }
     }
   }
 
