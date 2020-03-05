fcfa99037f2812058d2487fc0cd3c08ff70c4366
rorshaksama <cmd.rorshak@laposte.net>
2020-03-05 12:02:11 +0100

    Ajout des onglets

---
+++ src/app/recherche-entreprise/recherche-entreprise.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recherche-entreprise',
+  templateUrl: './recherche-entreprise.component.html',
+  styleUrls: ['./recherche-entreprise.component.css']
+})
+export class RechercheEntrepriseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
