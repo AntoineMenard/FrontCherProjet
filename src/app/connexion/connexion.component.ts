2ab17764722c39351fb2d9af75dc5276c7cc4c77
rorshaksama <cmd.rorshak@laposte.net>
2020-03-05 15:55:14 +0100

    ajout de connexion

--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Utilisateur } from '../model/Utilisateur';
 
 @Component({
   selector: 'app-connexion',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConnexionComponent implements OnInit {
 
+  
+  Utilisateur: Utilisateur = new Utilisateur();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-}
+  Connexion() {
+      localStorage.setItem(this.Utilisateur.mail, this.Utilisateur.mdp);
+      return true;
+    }
+  }
+
