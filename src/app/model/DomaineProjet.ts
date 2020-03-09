7bf6cceedcec33875372198166970a3cd6d93897
Tanguytlm <tanguy0789@gmail.com>
2020-03-09 19:13:57 +0100

    essais 400000 pour projet domaine

---
+++ src/app/model/DomaineProjet.ts
@@ -0,0 +1,12 @@
+import { Domaine } from './domaine';
+import { Projet } from './projet';
+
+export class DomaineProjet {
+
+    id: number;
+    domaine: Domaine;
+    projet: Projet;
+    constructor() {
+
+    }
+}
