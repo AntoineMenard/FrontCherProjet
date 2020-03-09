ac7747741b6bc3c99648ded3c084439dc111442c
Enyram <maryne.bensacoun@gmail.com>
2020-03-09 10:00:02 +0100

    recup id entrepise pour proposer projet

--- src/app/proposer-projet-entreprise/proposer-projet-entreprise.component.ts
+++ src/app/proposer-projet-entreprise/proposer-projet-entreprise.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Projet } from '../model/projet';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
+import { CherserviceService } from '../cherservice.service';
 
 @Component({
   selector: 'app-proposer-projet-entreprise',
@@ -13,13 +14,22 @@ export class ProposerProjetEntrepriseComponent implements OnInit {
   visible = false;
   p;
   Projet: Projet = new Projet();
+  entreprise;
 
   constructor(
     private router: Router,
     private http: HttpClient,
-    private dialog: MatDialog) { }
+    private dialog: MatDialog,
+    private myService: CherserviceService) { }
 
   ngOnInit(): void {
+
+    this.http.get(this.myService.lienHttp + 'entreprise/' + sessionStorage.getItem('idUtilisateur')).subscribe(data => {
+      this.entreprise = data;
+    }, err => {
+      console.log(err);
+    });
+
   }
   msgVisible() {
     if (this.visible === false) {
@@ -30,8 +40,8 @@ export class ProposerProjetEntrepriseComponent implements OnInit {
   }
 
   SoumettreProjet() {
+    this.Projet.entreprise = this.entreprise;
     this.http.post('http://localhost:8088/projet', this.Projet).subscribe(data => {
-        this.p = data;
         this.router.navigate(['/projets']);
     }, err => { console.log(err);
     });
