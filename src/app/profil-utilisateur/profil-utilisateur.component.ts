91f6efc431465e8989e60df3d63efa064069ff27
Enyram <maryne.bensacoun@gmail.com>
2020-03-09 14:53:25 +0100

    debut profil utilisateur

--- src/app/profil-utilisateur/profil-utilisateur.component.ts
+++ src/app/profil-utilisateur/profil-utilisateur.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CherserviceService } from '../cherservice.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { ModifProfilEntrepriseComponent } from '../modif-profil-entreprise/modif-profil-entreprise.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-profil-utilisateur',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilUtilisateurComponent implements OnInit {
 
-  constructor() { }
+  particulier;
+
+  constructor(
+
+    private router: Router,
+    private http: HttpClient,
+    public myService: CherserviceService,
+    private dialog: MatDialog
+
+  ) { }
 
   ngOnInit(): void {
+
+    this.http.get(this.myService.lienHttp + 'particulier/' + sessionStorage.getItem('idUtilisateur'))
+      .subscribe(data => {
+        this.particulier = data;
+      }, err => {
+        console.log(err);
+      });
+
   }
 
 }
