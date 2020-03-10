049da076d6009b1046ecdb370c51f75069b00e38
rorshaksama <cmd.rorshak@laposte.net>
2020-03-10 10:08:13 +0100

    valider

--- src/app/demande-participation-projet-utilisateur/demande-participation-projet-utilisateur.component.ts
+++ src/app/demande-participation-projet-utilisateur/demande-participation-projet-utilisateur.component.ts
@@ -2,42 +2,87 @@ import { Component, OnInit } from '@angular/core';
 import { CherserviceService } from '../cherservice.service';
 import { HttpClient } from '@angular/common/http';
 import { DemandeParticipation } from '../model/DemandeParticipation';
+import { Participation } from '../model/participation';
 @Component({
   selector: 'app-demande-participation-projet-utilisateur',
   templateUrl: './demande-participation-projet-utilisateur.component.html',
   styleUrls: ['./demande-participation-projet-utilisateur.component.css']
 })
 export class DemandeParticipationProjetUtilisateurComponent implements OnInit {
-
-  visible = false;
-  pro;
   part;
+  part2;
+  del;
+  entreprise;
+  projet;
+  participe;
   particulier;
+  particulier2;
+  idParticulier;
+  participation: Participation = new Participation();
   constructor(
     public myService: CherserviceService,
     private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get(this.myService.lienHttp + 'demandeParticipation').subscribe(data => {
-      this.particulier = data;
-      //console.log(data);
-    }, err => {
-      console.log(err);
-    })}
+    this.http.get(this.myService.lienHttp + 'demandeParticipation/entreprise/' + sessionStorage.getItem('idUtilisateur')
+    + '/' + sessionStorage.getItem('validerProjet') ).subscribe(data => {
+    this.part = data;
+    console.log(data);
+  }, err => {
+    console.log(err);
+  });
 
-consult(p){
-  this.http.get(this.myService.lienHttp + 'demandeParticipation/entreprise/' + sessionStorage.getItem('idUtilisateur')
-  + '/' + p.projet.idProjet ).subscribe(data => {
-  this.part = data;
+}
+
+supprimerCandidature(){
+  this.http.delete(this.myService.lienHttp
+    + 'demandeParticipation/entreprise/' + sessionStorage.getItem('validerProjet')).subscribe(data => {
+  this.del = data;
   console.log(data);
-  this.visible=true;
 }, err => {
   console.log(err);
 
 });
-
 }
 
+accepterCandidature(p){
+
+  //entreprise
+  this.http.get(this.myService.lienHttp + 'entreprise/' + sessionStorage.getItem('idUtilisateur')).subscribe(data => {
+    p.entreprise = data;
+    this.participation.entreprise = p.entreprise;
+    //console.log(this.participation.Entreprise);
+  }, err => {
+    console.log(err);
+  
+  });
+
+
+  //projet
+  this.http.get(this.myService.lienHttp + 'projet/' + sessionStorage.getItem('validerProjet')).subscribe(data => {
+    p.projet = data;
+    this.participation.projet = p.projet;
+    //console.log(this.participation.Projet);
+  }, err => {
+    console.log(err);
+  
+  });
+  
+  //console.log(p.entreprise);
+ 
+  this.participation.particulier = p.particulier;
+  console.log(this.participation);
+
+  this.http.post(this.myService.lienHttp + 'participation/', this.participation).subscribe(data => {
+    console.log(data);
+  }, err => {
+    console.log(err);
+  
+  });
+
+  //this.supprimerCandidature();
+
+}
 
 
 
