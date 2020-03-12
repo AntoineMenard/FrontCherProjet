69d0fa2715590f3a87b91d0d19058ad79c73ca0a
Tanguytlm <tanguy0789@gmail.com>
2020-03-12 10:48:43 +0100

    MODIF SECTEUR

--- src/app/proposer-projet-utilisateur/proposer-projet-utilisateur.component.ts
+++ src/app/proposer-projet-utilisateur/proposer-projet-utilisateur.component.ts
@@ -35,13 +35,18 @@ export class ProposerProjetUtilisateurComponent implements OnInit {
     private dialog: MatDialog,
     public myService: CherserviceService) { }
 
-    visible1 = false;
-    visible2 = false;
-    visible3 = false;
-    visible4 = false;
-    visible5 = true;
+  visible1 = false;
+  visible2 = false;
+  visible3 = false;
+  visible4 = false;
+  visible5 = true;
 
   ngOnInit(): void {
+    this.Domaine1.idDomaine = 0 ;
+    this.Domaine2.idDomaine = 0 ;
+    this.Domaine3.idDomaine = 0 ;
+    this.Domaine4.idDomaine = 0 ;
+    
     this.http.get(this.myService.lienHttp + 'domaine').subscribe(data => {
       this.domaines = data;
     }, err => {
@@ -74,49 +79,72 @@ export class ProposerProjetUtilisateurComponent implements OnInit {
   SoumettreProjet() {
 
     this.http.get<Domaine>(this.myService.lienHttp + 'domaine/' + this.Domaine.idDomaine)
-    .subscribe(data => {
-      //console.log(data);
-    this.DomainePro.domaine = data;    
-    this.http.get<Entreprise>(this.myService.lienHttp + 'entreprise/nom/' + this.Entre.nom)
-    .subscribe(data => {
-      //console.log(data);
-    this.entreprise = data;
-    this.projetpropose.entreprise = this.entreprise;
-    this.projetpropose.particulier = this.particulier; 
-    this.http.post< ProjetPropose >(this.myService.lienHttp + 'ProjetPropose', this.projetpropose).subscribe(data => {
-    this.DomainePro.projetpropose = data;
-    //console.log(this.DomainePro);
-    this.http.post(this.myService.lienHttp + 'domaineProjet', this.DomainePro)
-    // rempli le post avec null null alors que le console log le montre bien rempli et qu'aucune erreur n'est indiquée où que ce soit
-        .subscribe(data => {
-          //console.log(data); 
+      .subscribe(data => {
+        this.DomainePro.domaine = data;
+        this.http.get<Entreprise>(this.myService.lienHttp + 'entreprise/nom/' + this.Entre.nom)
+          .subscribe(data => {
+            this.entreprise = data;
+            this.projetpropose.entreprise = this.entreprise;
+            this.projetpropose.particulier = this.particulier;
+            this.http.post<ProjetPropose>(this.myService.lienHttp + 'ProjetPropose', this.projetpropose).subscribe(data => {
+              this.DomainePro.projetPropose = data;
+              this.gérerlessecteurs();
+
+
+            }
+              , err => { console.log(err); });
+
+          }, err => { console.log(err); });
+        this.router.navigate(['/recherche-projet']);
+      }, err => {
+        console.log(err);
+      });
+  }
+
+  rendreNextVisible() {
+    if (!this.visible1) { this.visible1 = true; } else {
+      if (!this.visible2) { this.visible2 = true; } else {
+        if (!this.visible3) { this.visible3 = true; } else {
+          if (!this.visible4) {
+            this.visible4 = true;
+            this.visible5 = false;
+          } else { }
         }
-        , err => {console.log(err); });
-
-}
-        , err => {console.log(err); });
-
-        }, err => { console.log(err); });
-    this.router.navigate(['/recherche-projet']);
-    }, err => { console.log(err);
-    });
-}
-
-rendreNextVisible() {
-  if (!this.visible1) { this.visible1 = true; } else {
-    if (!this.visible2) { this.visible2 = true; } else {
-      if (!this.visible3) { this.visible3 = true; } else {
-        if (!this.visible4) {
-          this.visible4 = true;
-          this.visible5 = false;
-        } else { }
       }
     }
   }
-}
 
-gérerlessecteurs(){
-  
-}
+  gérerlessecteurs() {
+    this.http.post(this.myService.lienHttp + 'domaineProjetPropose', this.DomainePro)
+      .subscribe(data => {
+        this.http.get<Domaine>(this.myService.lienHttp + 'domaine/' + this.Domaine1.idDomaine)
+          .subscribe(data => {
+            this.DomainePro.domaine = data;
+            this.http.post(this.myService.lienHttp + 'domaineProjetPropose', this.DomainePro)
+              .subscribe(data => {
+                this.http.get<Domaine>(this.myService.lienHttp + 'domaine/' + this.Domaine2.idDomaine)
+                  .subscribe(data => {
+                    this.DomainePro.domaine = data;
+                    this.http.post(this.myService.lienHttp + 'domaineProjetPropose', this.DomainePro)
+                      .subscribe(data => {
+                        this.http.get<Domaine>(this.myService.lienHttp + 'domaine/' + this.Domaine3.idDomaine)
+                          .subscribe(data => {
+                            this.DomainePro.domaine = data;
+                            this.http.post(this.myService.lienHttp + 'domaineProjetPropose', this.DomainePro)
+                              .subscribe(data => {
+                                this.http.get<Domaine>(this.myService.lienHttp + 'domaine/' + this.Domaine4.idDomaine)
+                                  .subscribe(data => {
+                                    this.DomainePro.domaine = data;
+                                    this.http.post(this.myService.lienHttp + 'domaineProjetPropose', this.DomainePro)
+                                      .subscribe(data => {
 
-}
+                                      }, err => { console.log(err); });
+                                    }, err => { console.log(err); });
+                                }, err => { console.log(err); });
+                            }, err => { console.log(err); });
+                        }, err => { console.log(err); });
+                    }, err => { console.log(err); });
+                }, err => { console.log(err); });
+            }, err => { console.log(err); });
+        }, err => { console.log(err); });
+}}
